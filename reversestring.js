868ef65052af3c5f5c38558b798efed8c2a284b0
Zahid Hasan <zahidhasan140338@gmail.com>
2023-08-08 02:14:44 +0600

    added fibonacci series

--- reversestring.js
+++ reversestring.js
@@ -3,7 +3,7 @@ function reverseString(text){
     for(let i = text.length-1; i>=0; i--){
         const element = text[i];
         reversed = reversed + element;
-        console.log(reversed);
+        // console.log(reversed);
     }
     return reversed;
 }
